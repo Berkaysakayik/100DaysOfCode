e317b26a8a0a7df676e102aba75eb6c20f96a234
Berkay <drops3305@gmail.com>
2023-07-21 01:38:15 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,11 @@
+num = int(input("Enter your number: "))
+print(num, "", type(num))
+
+floatNum = float(input("Enter your decimal number: "))
+print(floatNum, "", type(floatNum))
+
+string = str(input("Enter your characters: "))
+print (string, "", type(string))
+
+vallue = input("Enter whatever you want: ")
+print (vallue, "", type(vallue))
