f70f693f3b60d06f9bdba4447e15f79aa6a9b213
Berkay <drops3305@gmail.com>
2023-07-27 19:20:24 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -39,13 +39,62 @@ print("""
 ****************************************
 """)
 
-print("""
-We Provide Three Tours
-1. Tour Paris           (13 People)
-2. Tour Balkan          (21 People)
-3. Tour Egypt - Morocco (20 People)
+while True:
 
-""")
+    print("""
+    We Provide Three Tours
+    1. Tour Paris           (13 People)
+    2. Tour Balkan          (21 People)
+    3. Tour Egypt - Morocco (20 People)
+
+    """)
+
+
+    Package = int(input("Please Choose Package (1,2 or 3): "))
+
+    if Package == 1:
+        Person  = int(input("How many people are you be: "))
+        Student = int(input("How many students are you have: "))
+
+        if Person >=0 and Person <= 13:
+            if Student >=0 and Student <= 13:
+                cutstdnt = (Student * 3000)*(20/100)
+                stprice = (Student*3000) - cutstdnt
+                prsnprice = (Person-Student)*3000
+                totalprice = stprice+prsnprice
+                print(f"Your total bill is {totalprice} Euro. {stprice} Euro for students and {prsnprice} Euro is other peoples.")
+                break
+
+        if Person >= 13 and Person <= 21:
+            print("This tour is for only 13 people, if you are more than 13 you can look other tours.")
+
+        else:
+            print("Sorry our capasite is only for package's.")
+            break
+
+    if Package == 2:
+        Person  = int(input("How many people are you be: "))
+        Student = int(input("How many students are you have: "))
+
+        if Person >=0 and Person <= 21:
+            if Student >=0 and Student <= 13:
+                cutstdnt = (Student * 1200)*(20/100)
+                stprice = (Student*1200) - cutstdnt
+                prsnprice = (Person-Student)*1200
+                totalprice = stprice+prsnprice
+                print(f"Your total bill is {totalprice} Euro. {stprice} Euro for students and {prsnprice} Euro is other peoples.")
+                break
+        
+        else:
+            print("21 with the highest capacity of the tours we offer.")
+            break
+
+
+    
+
+    #Person  = int(input("How many people are you be: "))
+    #Student = int(input("How many students are you have: "))
+
 
 
 
