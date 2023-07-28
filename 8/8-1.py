5df24a2cb358fdad4252e74fed34704506b25d37
Berkay <drops3305@gmail.com>
2023-07-29 00:15:32 +0300

    Create 8-1.py

---
+++ 8/8-1.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+sabit = 12500
+sdeneyim = 100
+scocuk = 250
+smesai = 50
+
+print("""
+PersioN Hastanesi Maaş Hesaplama Programı 
+
+""")
+
+mesai =int(input("Lütfen bu ay yaptığınız mesai saatini giriniz: "))
+deneyim = int(input("Lütfen işinizdeki deneyiminizi yıl olarak giriniz: "))
+cocuk = int(input("Lütfen çocuğunuz var ise kaç çocuğunuz olduğunu giriniz(Yok ise 0 yazınız): "))
+
+hesapmaas = sdeneyim*deneyim + scocuk*cocuk + smesai*mesai + sabit
+print(f"Bu ay maaşınız {hesapmaas} TL olarak hesaplanmıştır.")
