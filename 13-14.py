413efdbdfaa5a5d76d5269427b85774b76b5fee8
Berkay <drops3305@gmail.com>
2023-08-03 18:24:38 +0300

    Create 13-14.py

---
+++ 13-14.py
@@ -0,0 +1,66 @@
+import tkinter as tk
+
+win = tk.Tk()
+
+def cal():
+    print()
+
+
+
+frm = tk.Frame(master=win)
+
+firstlabel = tk.Label(text= "PersioN Internet Data Consumption by Video Resolution")
+entrylabel = tk.Label(text= "Enter video length by minute (minute.second)")
+
+label_1    = tk.Label(text="")
+label_2    = tk.Label(text="")
+label_3    = tk.Label(text="")
+label_4    = tk.Label(text="")
+label_5    = tk.Label(text="")
+label_6    = tk.Label(text="")
+label_7    = tk.Label(text="")
+label_8    = tk.Label(text="")
+
+entry = tk.Entry(master=frm)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+win.mainloop()
