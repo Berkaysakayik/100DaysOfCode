456d5d732efeb83830ca0feafd6bbce607102495
Berkay <drops3305@gmail.com>
2023-08-04 16:19:38 +0300

    Create 15.py

---
+++ 15/15.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+data = pd.read_csv("iris.csv")
+
+print(data.head())                           # data setinin ilk 5 datasını gösterir
+#print(data.tail())                            # data setinin son 5 datasını gösterir
+print(data.isnull().sum())                 # csv içerisinde boş (null) data olup olmadığını kontrol et, varsa kaç adet olduğunu topla
+
+data.rename(columns={'SepalLengthCm':'Sepal_lengt_cm',
+                   'SepalWidthCm':'Sepal_width_cm',
+                   'PetalLengthCm':'Petal_length_cm',
+                   'PetalWidthCm':'Petal_width_cm',},inplace=True)
+
+print(data.head(15))                      # data setinin ilk parantez içerisi kadar datasını gösterir
+
+ 
+s= data['Species'].unique()           # datanın "Species" kolonunun farklı değerlerini listele
+
+for sp in s:
+    print("Species: ",sp)                 # listelenen veriyi yazdır
+
+
+Iris_setosa = data[data['Species'] == 'Iris-setosa'].value_counts().sum()    # datanın "Spieces" kolonunda kaç tane "Iris-setosa" olduğunu say ve kaç tane olduğunu topla
+print("How many we have Iris-setosa: ",Iris_setosa)
+
+# print(data[data['Species']=='Iris-setosa'])                                                 # bütün datadaki "Iris-setosa" ya ait verileri gösterir
+
+Iris_versicolor = data[data['Species'] == 'Iris-versicolor'].value_counts().sum()    # datanın "Spieces" kolonunda kaç tane "Iris-versicolor" olduğunu say ve kaç tane olduğunu topla
+print("How many we have Iris-versicolor: ",Iris_versicolor)
+
+Iris_virginica = data[data['Species'] == 'Iris-virginica'].value_counts().sum()    # datanın "Spieces" kolonunda kaç tane "Iris-virginica" olduğunu say ve kaç tane olduğunu topla
+print("How many we have Iris-virginica: ",Iris_virginica)
+
+
+colmn = data.columns                   # kaç tane sütun olduğunu bul
+
+for col in colmn:
+    print("Column: ",col)                  # sütunları yazdır
