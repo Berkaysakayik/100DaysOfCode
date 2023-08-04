7cd06bd905ea82a751d9d5d792ecf1a2cf118c4a
Berkay <drops3305@gmail.com>
2023-08-04 16:40:45 +0300

    Update 15.py

--- 15/15.py
+++ 15/15.py
@@ -10,7 +10,7 @@ print(data.head())                           # data setinin ilk 5 datasını gö
 #print(data.tail())                            # data setinin son 5 datasını gösterir
 print(data.isnull().sum())                 # csv içerisinde boş (null) data olup olmadığını kontrol et, varsa kaç adet olduğunu topla
 
-data.rename(columns={'SepalLengthCm':'Sepal_lengt_cm',
+data.rename(columns={'SepalLengthCm':'Sepal_length_cm',
                    'SepalWidthCm':'Sepal_width_cm',
                    'PetalLengthCm':'Petal_length_cm',
                    'PetalWidthCm':'Petal_width_cm',},inplace=True)
@@ -40,3 +40,31 @@ colmn = data.columns                   # kaç tane sütun olduğunu bul
 
 for col in colmn:
     print("Column: ",col)                  # sütunları yazdır
+
+
+"##################### GÖRSELSSSS #####################"
+
+
+sns.pairplot(data,hue='Species', diag_kind='hist')
+plt.show()
+
+
+sns.violinplot(x = 'Species', y ='Petal_length_cm', data = data)
+plt.title("petal length by species")
+plt.show()
+
+
+sns.swarmplot(x = 'Species', y ='Petal_length_cm', data = data)
+plt.title("petal length by species")
+plt.show()
+
+
+sns.barplot(x = 'Species', y ='Petal_length_cm', data = data)
+plt.title("petal length by species")
+plt.show()
+
+
+plt.figure(figsize=(16,7))
+x = plt.xticks(rotation=90)
+sns.kdeplot(data = data, x = "Sepal_length_cm", hue = 'Species', shade = True)
+plt.show()
