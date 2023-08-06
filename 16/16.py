b5995f1d3a7b0b653d6d46affd51cb0bd51da04b
Berkay <drops3305@gmail.com>
2023-08-06 22:15:35 +0300

    Create 16.py

---
+++ 16/16.py
@@ -0,0 +1,17 @@
+import numpy as np
+# print(help(np))
+
+# print(np.__version__)
+
+# numarray = np.arange(10)
+# print(numarray)
+
+# for i in range(len(numarray)):
+#     print(numarray[i])
+
+# print(help(np.full))
+
+np.full((3,3), True, dtype=bool)
+
+boolaray = np.full((3,3), True, dtype=bool)
+print("boolean array: \n",boolaray)
