e1ce1d318b98d4729ddab29706ae61a6b086a454
Berkay <drops3305@gmail.com>
2023-08-06 23:34:47 +0300

    Update 16.py

--- 16/16.py
+++ 16/16.py
@@ -1,9 +1,10 @@
 import numpy as np
+
 # print(help(np))
 
 # print(np.__version__)
 
-# numarray = np.arange(10)
+numarray = np.arange(10)
 # print(numarray)
 
 # for i in range(len(numarray)):
@@ -11,7 +12,55 @@ import numpy as np
 
 # print(help(np.full))
 
-np.full((3,3), True, dtype=bool)
+#""" boolaray - 1 """
 
-boolaray = np.full((3,3), True, dtype=bool)
-print("boolean array: \n",boolaray)
+# np.full((3,3), True, dtype=bool)
+
+# boolaray1 = np.full((3,3), True, dtype=bool)
+# print("boolean array 1: \n",boolaray1)
+
+
+#""" boolaray - 2 """
+
+# boolaray2 = np.full((9), True, dtype=bool).reshape(3,3)
+# print("boolean array 2: \n",boolaray2)
+
+
+#""" boolaray - 3 """
+
+# boolaray3 = np.ones((3,3),dtype=bool)
+# print(boolaray3)
+
+
+#""" boolaray - 4 """
+
+# boolaray4 = np.ones((27),dtype=bool).reshape(3,3,3)
+# print(boolaray4)
+
+
+# odd numbers = tek sayılar
+
+# numar = numarray[numarray%2 != 0]
+# print("\nnumpy odd numbers: ", numar)
+
+
+# even numbers = çift sayılar
+
+# numar = numarray[numarray%2 == 0]
+# print("\nnumpy even numbers: ", numar)
+
+
+# Numpy array de belirli bir durumu başka bir değer ile nasıl yer değiştirirsin
+
+# numarray[numarray%2 != 0] = -1
+# print("\nnodd numbers but modificated: ", numarray)
+
+
+# Numpy array de aslını değiştirmeden belirli bir durumu başka bir değer ile nasıl yer değiştirirsin
+
+# marr = numarray.copy()
+# marr[marr%2 == 1] = -1             # arrayimizdeki elemanların 2 ile bölümünde kalan var ise yerlerine -1 yazdırdık
+
+# marr[(marr % 5)!=  0 ] *=-1      # değişik bir şey
+
+# print(marr)
